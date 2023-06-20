3c01919fd29705e7dea3852da3022f98956de4a1
Victor Chepkasov <92711919+SugaryVictor@users.noreply.github.com>
2023-06-20 08:30:12 +0500

    update comments

--- contracts/savingsWallet.sol
+++ contracts/savingsWallet.sol
@@ -21,14 +21,17 @@ contract savingWallet {
         walletInfo.owner = payable(msg.sender);
     }
 
-    //устанавливаем лимит. Возможно, перенесу код в другую функцию
+    /*
+        Большая часть кода будет переписана, 
+        это лишь наброски того, как всё должно рбаотать
+    */
+
     function limit() public {
         walletInfo.weiPerDay = address(this).balance / 100;
         walletInfo.weiPerSecond = walletInfo.weiPerDay / uint(1 days);
         walletInfo.allowance = walletInfo.weiPerDay;
     }
 
-    //обновление лимита, скорее всего перепишу
     function update() public {
         walletInfo.allowance += walletInfo.weiPerSecond + (walletInfo.lastSendTime - block.timestamp);
         if (walletInfo.allowance > walletInfo.weiPerDay) {
