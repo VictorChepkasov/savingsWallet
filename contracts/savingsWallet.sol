3ddf76644dd0058ea636b6f24a506472f605b9fc
Victor Chepkasov <92711919+SugaryVictor@users.noreply.github.com>
2023-06-20 13:23:03 +0500

    add withdraw function

--- contracts/savingsWallet.sol
+++ contracts/savingsWallet.sol
@@ -1,19 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract savingWallet {
+contract SavingWallet {
     struct WalletInfo {
         address payable owner;
         address payable partyB;
-
-        uint weiPerDay;
-        uint weiPerSecond;
-        uint allowance; //оставшееся кол-во денег
-        uint lastSendTime;
-        //uint8 rewardB награда стороны B, устанавливается по желанию owner в процентах
-
+        uint weiPerDay; //wei на день
+        uint timeLeft; //оставшееся время до обновления лимита
+        /*uint8 rewardB награда стороны B, 
+        устанавливается по желанию owner в процентах*/
         bool partyBBad; //злонамернность стороны B
+        //оставшееся кол-во денег у сторон
     }
+    mapping(address => uint) allowances; 
 
     WalletInfo private walletInfo;
 
@@ -26,33 +25,36 @@ contract savingWallet {
         это лишь наброски того, как всё должно рбаотать
     */
 
-    function limit() public {
+    function setWalletInfo(address _partyB) public onlyOwner {
+        walletInfo.partyB = payable(_partyB);
         walletInfo.weiPerDay = address(this).balance / 100;
-        walletInfo.weiPerSecond = walletInfo.weiPerDay / uint(1 days);
-        walletInfo.allowance = walletInfo.weiPerDay;
-    }
-
-    function update() public {
-        walletInfo.allowance += walletInfo.weiPerSecond + (walletInfo.lastSendTime - block.timestamp);
-        if (walletInfo.allowance > walletInfo.weiPerDay) {
-            walletInfo.allowance = walletInfo.weiPerDay;
-        }
-        walletInfo.lastSendTime = block.timestamp;
-    }
-
-    function setWalletInfo(
-        address payable _partyB
-    ) public onlyOwner {
-        walletInfo.partyB = _partyB;
-        walletInfo.lastSendTime = block.timestamp;
         walletInfo.partyBBad = false;
+        updateLimit();
     }
 
-    function _withdraw(address _to, uint _value) private {
+    function withdraw(address _to, uint _value) public payable {
         require(msg.sender != address(0), "Wrong address!");
+        allowances[msg.sender] -= msg.value;
+        walletInfo.timeLeft = block.timestamp - walletInfo.timeLeft;
         (bool sent, ) = _to.call{value: _value}("");
         require(sent, "Failed to send Ether");
     }
+
+    function updateLimit() internal {
+        require(walletInfo.timeLeft - block.timestamp <= 0, "Time hasn't run out yet");
+        allowances[walletInfo.owner] = walletInfo.weiPerDay;
+        allowances[walletInfo.partyB] = walletInfo.weiPerDay;
+        walletInfo.timeLeft = block.timestamp;
+    }
+
+    function getSavingWalletInfo() public view returns(
+            WalletInfo memory, uint, uint
+        ) {
+        return (walletInfo, allowances[walletInfo.owner], allowances[walletInfo.partyB]);
+    }
+
+    receive() external payable {}
+    fallback() external payable {}
 
     modifier onlyOwner() {
         require(msg.sender == walletInfo.owner, 'Only Owner!');
